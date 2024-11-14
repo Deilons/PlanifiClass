62c80f896d8ecba98af569a2ed3607139e95c339
Deilons <apolo4748@gmail.com>
2024-11-14 10:19:43 -0500

    Feat: Models and migrations

---
+++ app/Models/ClassSession.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClassSession extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'date', 'duration', 'max_capacity', 'teacher_id'];
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class, 'enrollments');
+    }
+}
---
+++ app/Models/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function classSessions()
+    {
+        return $this->belongsToMany(ClassSession::class, 'enrollments');
+    }
+}
